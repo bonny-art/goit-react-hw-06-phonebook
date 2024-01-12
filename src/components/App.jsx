e9c86e38c97323b1cb333b2d1da76d12ef5e64b9
bonny-art <svitlana.otenko@gmail.com>
2024-01-13 01:29:49 +0200

    adds files for HW from previous HW

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,62 @@
+import React, { useEffect, useState } from 'react';
+import { nanoid } from 'nanoid';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+import { PhoneInputForm, ContactsList, Filter } from 'components';
+import { Section, Header, Title } from './Section/Section.styled';
+
+const LS_KEY = 'phone_contacts';
+
 export const App = () => {
+  const [contacts, setContacts] = useState(() => {
+    return JSON.parse(localStorage.getItem(LS_KEY)) || [];
+  });
+  const [filter, setFilter] = useState('');
+
+  const addContact = ({ name, number }) => {
+    const contact = {
+      id: nanoid(),
+      name,
+      number,
+    };
+
+    const isExist = contacts.find(({ name }) => name === contact.name.trim());
+
+    if (isExist) {
+      Notify.failure(`${contact.name} is already in contacts.`);
+      return;
+    }
+
+    return setContacts(prev => [contact, ...prev]);
+  };
+
+  useEffect(() => {
+    localStorage.setItem(LS_KEY, JSON.stringify(contacts));
+  }, [contacts]);
+
+  const deleteContact = contactId => {
+    setContacts(prev => prev.filter(({ id }) => contactId !== id));
+  };
+
+  const changeFilter = e => {
+    setFilter(e.currentTarget.value);
+  };
+
+  const normalizedFilter = filter.toLowerCase();
+  const visibleContacts = contacts.filter(contact =>
+    contact.name.toLowerCase().includes(normalizedFilter)
+  );
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Section>
+      <Header>Phonebook</Header>
+      <PhoneInputForm onSubmit={addContact} />
+      <Title>Contacts</Title>
+      <Filter onChange={changeFilter} value={filter} />
+      <ContactsList
+        contacts={visibleContacts}
+        onDeleteContact={deleteContact}
+      />
+    </Section>
   );
 };
