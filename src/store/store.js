67fb763728af5db2ceae4ef0b648eaaae6734726
bonny-art <svitlana.otenko@gmail.com>
2024-01-14 18:22:12 +0200

    solves the issue with the serialization error

--- src/store/store.js
+++ src/store/store.js
@@ -1,5 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { persistStore, persistReducer } from 'redux-persist';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 
 import { contactsReducer, filterReducer } from '../store';
@@ -16,5 +25,13 @@ const reducer = {
   contacts: persistedReducer,
 };
 
-export const store = configureStore({ reducer });
+export const store = configureStore({
+  reducer,
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
 export const persistor = persistStore(store);
